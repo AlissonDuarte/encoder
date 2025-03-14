2c009a1234a26e0e32bcdedd9716b827081abe07
AlissonDuarte <alissonsilvamorimduarte@gmail.com>
2025-03-14 01:54:27 -0300

    Cleaning prints and logs

--- application/services/job_services.go
+++ application/services/job_services.go
@@ -98,26 +98,17 @@ func (j *JobService) performUpload() error {
 	}
 
 	videoUpload := NewVideoUpload()
-	fmt.Println("criando new video upload")
 	videoUpload.OutPutBucket = os.Getenv("outputBucketName")
-	fmt.Printf("OutPutBucket setado como : %s\n", videoUpload.OutPutBucket)
 	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + j.Job.Video.ID
-	fmt.Printf("VideoPath setado como : %s\n", videoUpload.VideoPath)
 	concurrency, _ := strconv.Atoi(os.Getenv("CONCURRENCY"))
-	fmt.Printf("concurrency setado como : %d\n", concurrency)
 	doneUpload := make(chan string)
 
-	fmt.Println("criando go rotine")
 	go videoUpload.ProcessUpload(concurrency, doneUpload)
-	fmt.Println("go rotine criada")
 
-	fmt.Println("esperando resultado")
 	uploadResult := <-doneUpload
-	fmt.Printf("Resultado: %s\n", uploadResult)
 	if uploadResult != "uploaded completed" {
 		return j.failJob(err)
 	}
-	fmt.Println("finalizando processo de upload")
 	return err
 }
 func (j *JobService) changeJobStatus(status string) error {
